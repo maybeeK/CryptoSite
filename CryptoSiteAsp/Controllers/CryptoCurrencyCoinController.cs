df4c1d2d8400d97c314b46f26c42d888f881f19e
maybeeK <rom.bosyj@gmail.com>
2023-04-03 15:24:10 +0300

    Added possibility to check info about every existing coin

---
+++ CryptoSiteAsp/Controllers/CryptoCurrencyCoinController.cs
@@ -0,0 +1,27 @@
+﻿using CryptoSiteAsp.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CryptoSiteAsp.Controllers
+{
+	public class CryptoCurrencyCoinController : Controller
+	{
+		private readonly ICryptoService _cryptoService;
+		public CryptoCurrencyCoinController(ICryptoService cryptoService)
+		{
+			_cryptoService = cryptoService;
+		}
+
+		public async Task<IActionResult> Index(string coinSymbol)
+		{
+			var coin = await _cryptoService.GetCurrencyBySymbol(coinSymbol);
+			if (coin == null)
+			{
+				return BadRequest();
+			}
+			else
+			{
+				return View(coin);
+			}
+		}
+	}
+}
