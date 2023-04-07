97fb950a87a517f9fcf9d3f3dc7bb50fda870712
maybeeK <rom.bosyj@gmail.com>
2023-04-07 16:01:40 +0300

    Added possibility get&load candles to user

---
+++ CryptoSiteAsp/wwwroot/js/LoadCandles.js
@@ -0,0 +1,21 @@
+﻿
+    $(document).ready(function () {
+        var symbol = $("#cryptoCurrencyCandles").attr("name")
+        var url = "CryptoCurrencyCoin/GetCandles?coinSymbol=" + symbol;
+        if (symbol!=null) {
+            $.ajax({
+                type: "GET",
+                url: url,
+                success: function (data) {
+                    $("#cryptoCurrencyCandles").html(data);
+                },
+                error: function (xhr, ajaxOptions, thrownError) {
+                    var errorText = "Status: " + xhr.status + " - " + xhr.statusText;
+
+                    PresentClosableBootstrapAlert("#alert_placeholder_login", "danger", "Error!", errorText);
+
+                    console.error(thrownError + "\r\n" + xhr.statusText + "\r\n" + xhr.responceText);
+                }
+            });
+        }
+    });
